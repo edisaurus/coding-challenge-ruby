dcffed1798b7f88734744b65de5d77b48853169f
edisaurus <edswork.ece@gmail.com>
2021-05-11 11:04:30 +0800

    initialize first working test

--- lib/exam.rb
+++ lib/exam.rb
@@ -1,3 +1,12 @@
 class Bakery
-    
+  attr_accessor :number_of_items, :item_code
+  
+  def initialize(number_of_items, item_code)
+    @number_of_items = number_of_items
+    @item_code = item_code
+  end
+end
+
+def add(a,b)
+    a + b
 end
--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -1,5 +1,7 @@
 require "exam"
 
 RSpec.describe "Bakery" do
-    
+    it "add numbers" do
+        expect(add(3,1)).to eq 4
+    end
 end
